54a6d8b8f3db2f866494add083cb473d3b3508b1
ngarrido777 <ngarrido@milaifontanals.org>
2024-04-30 17:51:51 +0200

    Test commit

---
+++ sql_scripts/script2_dades.sql
@@ -0,0 +1,8 @@
+-- Usuarios
+    -- 2 Amins
+    -- 2 No admins
+INSERT INTO usuaris VALUES 
+    (NULL,'ngarrido','1234',1),
+    (NULL,'dcano','1234',1),
+    (NULL,'aparera','1234',0),
+    (NULL,'acarrillo','1234',0),
