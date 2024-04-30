b529a0428789a6f6306d3da03599737a8b599f4e
david <dcano@milaifontanals.org>
2024-04-30 20:10:53 +0200

    Edicion Sript 2

--- sql_scripts/script2_dades.sql
+++ sql_scripts/script2_dades.sql
@@ -1,8 +1,23 @@
--- Usuarios
+-- Usuaris
     -- 2 Amins
     -- 2 No admins
 INSERT INTO usuaris VALUES 
     (NULL,'ngarrido','1234',1),
     (NULL,'dcano','1234',1),
     (NULL,'aparera','1234',0),
-    (NULL,'acarrillo','1234',0),
+    (NULL,'acarrillo','1234',0);
+    
+-- Estat Curses
+    -- 6 estats
+INSERT INTO estat_cursa VALUES
+    (NULL, 'En preparació'),
+    (NULL, 'Inscripció Oberta'),
+    (NULL, 'Inscripció Tancada'),
+    (NULL, 'En curs'),
+    (NULL, 'Finalitzada'),
+    (NULL, 'Cancelada');
+    
+-- Esports
+   --
+INSERT INTO esport VALUES
+    ()
