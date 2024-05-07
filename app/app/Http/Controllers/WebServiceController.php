2d8c79c9ac32c3c404d23b4ae759673b203115c1
Tu nomrbe <tu@correo.aqui>
2024-05-07 17:14:28 +0200

    Commit web service

--- app/app/Http/Controllers/WebServiceController.php
+++ app/app/Http/Controllers/WebServiceController.php
@@ -7,39 +7,50 @@ use App\Models\Cursa;
 
 class WebServiceController extends Controller
 {
-    public function getCurses()
+    public function getCurses(Request $request)
     {
         //Obtebir curses
         $curses = Cursa::with('esport', 'estat')->get();
-        //Omplir el json amb les dades de la cursa
-        foreach ($curses as $cursa) {
-            $cursesArray[] = [
-                "id" => $cursa->cur_id,
-                "nom" => $cursa->cur_nom,
-                "dataInici" => $cursa->cur_data_inici,
-                "dataFi" => $cursa->cur_data_fi,
-                "lloc" => $cursa->cur_lloc,
-                "esport" => [
-                    "id" => $cursa->esport->esp_id,
-                    "nom" => $cursa->esport->esp_nom
-                ],
-                "estat" => [
-                    "id" => $cursa->estat->est_id,
-                    "nom" => $cursa->estat->est_nom
-                ],
-                "descripcio" => $cursa->cur_desc,
-                "limit" => $cursa->cur_limit_inscr,
-                "foto" => $cursa->cur_foto,
-                "web" => $cursa->cur_web
+        //Declarar json de curses i error
+        $cursesArray = [];
+        $error = [];
+        if(!empty($curses)){
+            //Omplir el json amb les dades de la cursa
+            foreach ($curses as $cursa) {
+                $cursesArray = [
+                    "id" => $cursa->cur_id,
+                    "nom" => $cursa->cur_nom,
+                    "dataInici" => $cursa->cur_data_inici,
+                    "dataFi" => $cursa->cur_data_fi,
+                    "lloc" => $cursa->cur_lloc,
+                    "esport" => [
+                        "id" => $cursa->esport->esp_id,
+                        "nom" => $cursa->esport->esp_nom
+                    ],
+                    "estat" => [
+                        "id" => $cursa->estat->est_id,
+                        "nom" => $cursa->estat->est_nom
+                    ],
+                    "descripcio" => $cursa->cur_desc,
+                    "limit" => $cursa->cur_limit_inscr,
+                    "foto" => "foto"/*$cursa->cur_foto*/,
+                    "web" => $cursa->cur_web
+                ];
+            }
+            $error = [
+                "code" => "200",
+                "description" => "Ok"
+            ];
+        }else{
+            $error = [
+                "code" => "200",
+                "description" => "Ok"
             ];
         }
         //Enviar json amb els errors i les curses
         return response()->json([
             "curses" => $cursesArray,
-            "response" => [
-                "code" => "200",
-                "description" => "Todo piola"
-            ]
+            "response" => $error,
         ]);
     }
 
