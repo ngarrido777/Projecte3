dd7edd6bdc60a6453aa99207d644297f0579da1a
dcanoproj <canodomingoa@gmail.com>
2024-05-08 15:23:34 +0200

    Comit de recuperacion parcial de las perdidas del Proyecto 3

--- app/app/Http/Controllers/CreaciocursaController.php
+++ app/app/Http/Controllers/CreaciocursaController.php
@@ -10,6 +10,18 @@ class CreaciocursaController extends Controller
 {
     public function creaciocurses()
     {
+        //Array de errors
+        $errors = array(
+            'e_nom' => '',
+            'e_data_inici' => '',
+            'e_data_fi' => '',
+            'e_lloc' => '',
+            'e_esport' => '',
+            'e_descripcio' => '',
+            'e_limit' => '',
+            'e_foto' => '',
+            'e_web' => ''
+        );
         //Tractament del post
         if(isset($_POST["c_crear"]))
         {
@@ -18,19 +30,17 @@ class CreaciocursaController extends Controller
             $nom = $_POST["c_nom"];
             if(strlen($nom) > 50 || strlen($nom) <= 0)
             {
-                //Error
+                $errors['e_nom'] = 'La mida del nom no es correcte';
             }
             //Validar data inici
             $data_inici = $_POST["c_data_inici"];
-            //TO DO validar c_data_inici
             //Validar data fi
             $data_fi = $_POST["c_data_fi"];
-            //TO DO validar c_data_fi
             //Validar lloc
             $lloc = $_POST["c_lloc"];
             if(strlen($lloc) > 20 || strlen($lloc) <= 0)
             {
-                //Error
+                $errors['e_lloc'] = 'La mida del lloc no es correcte';
             }
             //Validar esport
             $id_esport = $_POST["c_esport"];
@@ -39,20 +49,20 @@ class CreaciocursaController extends Controller
             $descripccio = $_POST["c_descripccio"];
             if(strlen($descripccio) > 1000 || strlen($descripccio) <= 0)
             {
-                //Error
+                $errors['e_descripccio'] = 'La mida de la descripccio no es correcte';
             }
             //Validar limit inscrits
             $limit = $_POST["c_limit"];
-            if(is_numeric($limit))
+            if(!is_numeric($limit))
             {
-                //Error
+                $errors['e_limit'] = 'El limit ha de ser numeric';
             }
             //TO DO tractar i validar imatge :(
             //Validar web
             $web = $_POST["c_web"];
             if(strlen($web) > 200 || strlen($web) <= 0)
             {
-                //Error
+                $errors['e_web'] = 'La mida de la web no es correcte';
             }
         }
         //Carregar els esports per la view
@@ -64,6 +74,7 @@ class CreaciocursaController extends Controller
         //return a la view amb la variable esports
         return view('creaciocurses', [
             'esports' => $esp_names,
+            'errors' => $errors
         ]);
     }
 }
--- app/resources/views/creaciocurses.blade.php
+++ app/resources/views/creaciocurses.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Creacio Curses</title>
     <style>
-        .form-element {
+        .error {
             display: block;
             margin-bottom: 20px;
+
+            color: red;
         }
     </style>
 
@@ -17,31 +19,40 @@
     {{ Form::open(['url' => 'creaciocurses', 'method' => 'post']) }}
         @csrf
         {{ Form::label('l_nom', 'Nom:') }}
-        {{ Form::text('c_nom', null, ['class' => 'form-element form-text']) }}
+        {{ Form::text('c_nom', null) }}
+        {{ Form::label(null, $errors['e_nom'], ['class' => 'error']) }}
 
         {{ Form::label('l_data_inici', 'Data Inici:') }}
-        {{ Form::date('c_data_inici', null, ['class' => 'form-element form-text']) }}
+        {{ Form::date('c_data_inici', null) }}
+        {{ Form::label(null, $errors['e_data_inici'], ['class' => 'error']) }}
         
         {{ Form::label('l_data_fi', 'Data Fi:') }}
-        {{ Form::date('c_data_fi', null, ['class' => 'form-element form-text']) }}
+        {{ Form::date('c_data_fi', null) }}
+        {{ Form::label(null, $errors['e_data_fi'], ['class' => 'error']) }}
 
         {{ Form::label('l_lloc', 'Lloc:') }}
-        {{ Form::text('c_lloc', null, ['class' => 'form-element form-text']) }}
+        {{ Form::text('c_lloc', null) }}
+        {{ Form::label(null, $errors['e_lloc'], ['class' => 'error']) }}
 
         {{ Form::label('l_esport', 'Esport:') }}
-        {{ Form::select('c_esport', $esports, null, ['class' => 'form-element form-text']) }}
+        {{ Form::select('c_esport', $esports, null) }}
+        {{ Form::label(null, $errors['e_esport'], ['class' => 'error']) }}
 
         {{ Form::label('l_descripccio', 'Descripccio:') }}
-        {{ Form::text('c_descripccio', null, ['class' => 'form-element form-text']) }}
+        {{ Form::text('c_descripccio', null) }}
+        {{ Form::label(null, $errors['e_descripcio'], ['class' => 'error']) }}
 
         {{ Form::label('l_limit', 'Limit:') }}
-        {{ Form::text('c_limit', null, ['class' => 'form-element form-text']) }}
+        {{ Form::text('c_limit', null) }}
+        {{ Form::label(null, $errors['e_limit'], ['class' => 'error']) }}
 
         {{ Form::label('l_foto', 'Foto:') }}
-        {{ Form::file('c_foto', null, ['class' => 'form-element form-text']) }}
+        {{ Form::file('c_foto', null) }}
+        {{ Form::label(null, $errors['e_foto'], ['class' => 'error']) }}
         
         {{ Form::label('l_web', 'Web:') }}
-        {{ Form::text('c_web', null, ['class' => 'form-element form-text']) }}
+        {{ Form::text('c_web', null) }}
+        {{ Form::label(null, $errors['e_web'], ['class' => 'error']) }}
 
         {{ Form::submit('Crear', ['name' => 'c_crear']) }}
     {{ Form::close() }}
